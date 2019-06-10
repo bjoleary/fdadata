3301ab6aca41481999ac466eafc288e91ddb1c09
Brendan O'Leary <brendan@boleary.com>
2019-06-09 23:46:11 -0400

    Bug Fix: Remove incorrect readr:: call for base::readLines
    Should have run my tests before commit...

--- R/clean_raw_text_file.R
+++ R/clean_raw_text_file.R
@@ -6,7 +6,7 @@
 #'
 clean_raw_text_file <- function(filepath){
   message("Cleaning string from ", filepath, sep = "")
-  dirty_string <- readr::readLines(filepath, skipNul = TRUE)
+  dirty_string <- readLines(filepath, skipNul = TRUE)
   dirty_string <- gsub("\"", "", readr::read_lines(dirty_string, skip = 1))
   clean_string <- dirty_string
 }
