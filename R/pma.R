8d6997d1f71c59f1b2f00f3687a26858b044feed
Brendan O'Leary <brendan@boleary.com>
2026-04-21 13:35:48 -0400

    Update data.

--- R/pma.R
+++ R/pma.R
@@ -5,7 +5,7 @@
 #'
 #' Get the latest data using \code{fdadata::etl_pma()}.
 #'
-#' @format A tibble with 56159 rows and 25 fields:
+#' @format A tibble with 56206 rows and 25 fields:
 #'
 #' \describe{
 #' \item{submission_number}{chr "N18143/S001", "N50009", "N50042", "N5016…}
@@ -37,5 +37,5 @@
 #' }
 #'
 #' @source [FDA PMA Download File](https://www.fda.gov/medical-devices/device-approvals-denials-and-clearances/pma-approvals)
-#' accessed 2026-04-14.
+#' accessed 2026-04-21.
 "pma"
