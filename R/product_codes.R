90d3674c6baef2bbdf99ca49f4b04d9679258888
Brendan O'Leary <brendan@boleary.com>
2026-08-11 13:11:27 -0400

    Refresh FDA data
    Auto-merged by sage fdadata.service after the required R-CMD-check jobs passed.

--- R/product_codes.R
+++ R/product_codes.R
@@ -5,7 +5,7 @@
 #'
 #' Get the latest data using \code{fdadata::etl_procodes()}.
 #'
-#' @format A tibble with 7085 rows and 17 fields:
+#' @format A tibble with 7087 rows and 17 fields:
 #'
 #' \describe{
 #' \item{review_panel}{fct "Anesthesiology", "Anesthesiology", …}
@@ -29,5 +29,5 @@
 #' }
 #'
 #' @source [FDA Product Code Database]()
-#' accessed 2026-08-09.
+#' accessed 2026-08-11.
 "product_codes"
